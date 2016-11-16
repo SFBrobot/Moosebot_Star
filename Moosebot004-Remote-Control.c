8cc5eff1c4c4242c03af3775a9b0da50a859d268
GS501 <gsev501@gmail.com>
2016-11-16 15:25:58 -0800

    Update Moosebot004-Remote-Control.c

--- Moosebot004-Remote-Control.c
+++ Moosebot004-Remote-Control.c
@@ -17,77 +17,79 @@ void standard(int pwrA, int pwrB, int s) { // Function for the standard drive of
 	motor[rfWheel] = motor[rbWheel] = pwrB;
 	wait1Msec(s);
 }
-void arm(int pwr, int s) { // Function for the arm of
+void arm(int pwr, int s) { // Function for the arm of the robot.
 	motor[aMotor] = motor[bMotor] = motor[cMotor] = motor[dMotor] = pwr;
 	wait1Msec(s);
 }
-void clawMovement(int pwr, int s) {
+//Bottom motors on each side are inverted. Top one is not.
+
+void clawMovement(int pwr, int s) { // Function for the clawe of the robot.
 	motor[lClaw] = pwr;
 	motor[rClaw] = pwr;
 	wait1Msec(s);
 }
 
-void pre_auton() { }
+void pre_auton() { } // Function to set values before the match starts
 
-task autonomous() {
+task autonomous() { // Function that runs the autonomous program.
 
 	clawMovement(-127, 300); // Opens the claw.
-	clawMovement(0, 200);
-	arm(127, 200);
-	arm(0, 350);
-	arm(-127, 100);
-	arm(0, 350);
-	standard(127, 127, 1400);
-	standard(0, 0, 350);
-	clawMovement(127, 850);
-	clawMovement(0, 300);
-	standard(-127, -70, 500);
-	standard(0, 0, 200);
-	standard(-127, 127, 345);
-	standard(0, 0, 200);
-	standard(-127, -127, 2000);
-	standard(0, 0, 200);
-	arm(127, 1500);
-	arm(0, 200);
-	clawMovement(127, 750);
-	clawMovement(0, 250);
-	clawMovement(-127, 750);
-	clawMovement(0, 250);
-	arm(-127, 1500);
-	arm(0, 250);
+	clawMovement(0, 200); // Stops the claw.
+	arm(127, 200); // Raises the arm up.
+	arm(0, 350); // Stops the arm.
+	arm(-127, 100); // Lowers the arm.
+	arm(0, 350); // Stops the arm.
+	standard(127, 127, 1400); // Moves the robot forward.
+	standard(0, 0, 350); // Stops the robot.
+	clawMovement(127, 850); // Closes that claw.
+	clawMovement(0, 300); // Stops the claw.
+	standard(-127, -70, 500); // Moves the robot backwards while slightly turning left.
+	standard(0, 0, 200); // Stops the robot.
+	standard(-127, 127, 345); // Turns the robot left a bit.
+	standard(0, 0, 200); // Stops the robot.
+	standard(-127, -127, 2000); // Moves the robot backwards.
+	standard(0, 0, 200); // Stops the robot.
+	arm(127, 1500); // Raises the arm.
+	arm(0, 200); // Stops the arm.
+	clawMovement(127, 750); // Opens the claw.
+	clawMovement(0, 250); // Stops the claw.
+	clawMovement(-127, 750); // Closes the claw.
+	clawMovement(0, 250); // Stops the 
+	arm(-127, 1500); // Lowers the arm.
+	arm(0, 250); // Stops the arm.
 
 }
 
-task usercontrol()
+task usercontrol() // Function for user control.
 {
-	while(true) {
-		motor[lfWheel] = motor[lbWheel] = vexRT[Ch3] + vexRT[Ch1];
+	while(true) { // Loops the code until the map is over.
+		motor[lfWheel] = motor[lbWheel] = vexRT[Ch3] + vexRT[Ch1]; // Formula for arcade.
 		motor[rfWheel] = motor[rbWheel] = vexRT[Ch3] - vexRT[Ch1];
-	if(vexRT[Btn6U] == 1) {
+	if(vexRT[Btn6U] == 1) { // Checks if a button is pressed, and if so, opens the claw.
 		motor[lClaw] = 127;
 		motor[rClaw] = 127;
 }
-	else if(vexRT[Btn6D] == 1) {
+	else if(vexRT[Btn6D] == 1) { // Checks if a button is pressed, and if so, closes the claw.
 		motor[lClaw] = -127;
 		motor[rClaw] = -127;
 }
-	else {
+	else { // Checks if no buttons are pressed, and if so, stops the claw.
 	motor[lClaw] = 0;
 	motor[rClaw] = 0;
 }
-		if(vexRT[Btn5U] == 1) {
+		if(vexRT[Btn5U] == 1) { // Checks if a button is pressed, and if so, raises the arm.
 			motor[aMotor] = 127;
 			motor[bMotor] = 127;
 			motor[cMotor] = 127;
 			motor[dMotor] = 127;
 	}
-		else if(vexRT[Btn5D] == 1) {
+		else if(vexRT[Btn5D] == 1) { // Checks if a button is pressed, and if so, lowers the arm.
 			motor[aMotor] = -127;
 			motor[bMotor] = -127;
 			motor[cMotor] = -127;
 			motor[dMotor] = -127;
 	}
-		else {
+		else { // Checks if a button is pressed, and if so, stops the arm.
 			motor[aMotor] = 0;
 			motor[bMotor] = 0;
 			motor[cMotor] = 0;
