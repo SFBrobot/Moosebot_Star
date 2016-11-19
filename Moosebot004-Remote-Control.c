39845beefb85ce38194bf0eaeab5ecbe069c6cb6
GS501 <gsev501@gmail.com>
2016-11-19 11:31:02 -0800

    Update Moosebot004-Remote-Control.c

--- Moosebot004-Remote-Control.c
+++ Moosebot004-Remote-Control.c
@@ -39,33 +39,22 @@ task autonomous() { // Function that runs the autonomous program.
 	arm(0, 350); // Stops the arm.
 	arm(-127, 100); // Lowers the arm.
 	arm(0, 350); // Stops the arm.
-	standard(127, 127, 1400); // Moves the robot forward.
-	standard(0, 0, 350); // Stops the robot.
-	clawMovement(127, 950); // Closes that claw.
-	clawMovement(0, 300);
-	clawMovement(50, 250);
+	standard(127, 127, 1000);
+	standard(0, 0, 250);
+	standard(-127, 127, 250);
+	standard(0, 0, 250);
+	clawMovement(127, 900);
 	clawMovement(0, 250);
-	standard(-127, -70, 250); // Moves the robot backwards while slightly turning left.
-	standard(0, 0, 200); // Stops the robot.
-	clawMovement(127, 400);
-	clawMovement(0, 100);
-	standard(-127, -70, 250); // Moves the robot backwards while slightly turning left.
-	standard(0, 0, 200); // Stops the robot.
-	clawMovement(127, 400);
-	clawMovement(0, 100);
-	standard(-127, 127, 345); // Turns the robot left a bit.
-	standard(0, 0, 200); // Stops the robot.
-	standard(-127, -127, 2000); // Moves the robot backwards.
-	standard(0, 0, 200); // Stops the robot.
-	arm(127, 1500); // Raises the arm.
-	arm(0, 200); // Stops the arm.
-	clawMovement(127, 750); // Opens the claw.
-	clawMovement(0, 250); // Stops the claw.
-	clawMovement(-127, 750); // Closes the claw.
-	clawMovement(0, 250); // Stops the 
-	arm(-127, 1500); // Lowers the arm.
-	arm(0, 250); // Stops the arm.
-
+	standard(-127, 127, 750);
+	standard(0, 0, 250);
+	standard(-127, -127, 2000);
+	standard(0, 0, 250);
+	arm(127, 1200);
+	arm(0, 0, 300);
+	clawMovement(127, 250);
+	clawMovement(0, 250);
+	arm(-127, 600);
+	arm(0, 400);
 }
 
 task usercontrol() // Function for user control.
