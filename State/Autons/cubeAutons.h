f3539e968437796956660ecc9f7f57661a394bb2
l4cuatro <lpieroni@sfhighschool.net>
2017-02-22 08:10:42 -0800

    Update cubeAutons.h

--- State/Autons/cubeAutons.h
+++ State/Autons/cubeAutons.h
@@ -1,15 +1,4 @@
 
-
-void deployClaw() {
-	setLift(127);
-	wait1Msec(400);
-	setLift(0);
-	wait1Msec(20);
-	setLift(-127);
-	wait1Msec(250);
-	setLift(0);
-}
-
 void redCubeAuton() {
 	deployClaw();
 	wait1Msec(250);
